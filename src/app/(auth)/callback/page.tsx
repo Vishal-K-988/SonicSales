83b203b5a578cfe40e28894f41d3f217cc529455
vishal <vishalkumarvkvk988@gmail.com>
2025-05-19 19:36:49 +0530

    Statup and Clerk setup done
    Signed-off-by: vishal <vishalkumarvkvk988@gmail.com>

---
+++ src/app/(auth)/callback/page.tsx
@@ -0,0 +1,11 @@
+// Todo : implement the auth callback page 
+
+export const dynamic = "force-dynamic"
+
+const AuthCallbackPage = () => {
+    return <div>
+        AuthCallbackPage 
+    </div>
+}
+
+export default AuthCallbackPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Press_Start_2P } from "next/font/google";
+import { ThemeProvider } from "@/provider/theme-provider"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const pressStart2P = Press_Start_2P({
+  variable: "--font-press-start-2p",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -23,12 +29,37 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+
+    <ClerkProvider>
+
+
+
+    <html lang="en"  suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${pressStart2P.variable} antialiased`}
       >
+         <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+       
         {children}
+
+        </ThemeProvider>
+     
       </body>
     </html>
+    </ClerkProvider>
   );
 }
